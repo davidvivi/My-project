55232d6202e8cbcdc563eeea484dc30eb0c0ab04
krisz <646904527@qq.com>
2016-07-15 19:52:44 +0800

    config

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -15,14 +15,14 @@ return array(
 
     //Auth配置
     'AUTH_CONFIG' => array(
-/*        // 用户组数据表名
+      // 用户组数据表名
         'AUTH_GROUP' => 'think_auth_group',
         // 用户-用户组关系表
         'AUTH_GROUP_ACCESS' => 'think_auth_group_access',
         // 权限规则表
-        'AUTH_RULE' => 'think_auth_rule',*/
+        'AUTH_RULE' => 'think_auth_rule',
         // 用户信息表
-        'AUTH_USER' => 'user',
+        'AUTH_USER' => 'think_admin',
 
     ),
     ),
