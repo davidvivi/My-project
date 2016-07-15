4e55db19e64fd8a3b610145216277390cee2cb14
krisz <646904527@qq.com>
2016-07-15 22:42:02 +0800

    YEMIAN

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -1,8 +1,13 @@
 <?php
-namespace Home\Controller;
+
+namespace Admin\Controller;
+
 use Think\Controller;
+
 class IndexController extends Controller {
-    public function index(){
-        
+
+    public function index()
+    {
+        $this->display('index/index');
     }
 }
