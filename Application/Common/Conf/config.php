d9424d0eeb0308b5577bacb52ab07ca563d3fa19
huanghaoran <6911293011@qq.com>
2016-07-15 19:37:50 +0800

    config

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -10,4 +10,19 @@ return array(
     'DB_PWD'                =>  '123',          // 密码
     'DB_PORT'               =>  '',        // 端口
     'DB_PREFIX'             =>  'think_',    // 数据库表前缀
+	
+	'SHOW_PAGE_TRACE' =>true, 
+
+    //Auth配置
+    'AUTH_CONFIG' => array(
+/*        // 用户组数据表名
+        'AUTH_GROUP' => 'think_auth_group',
+        // 用户-用户组关系表
+        'AUTH_GROUP_ACCESS' => 'think_auth_group_access',
+        // 权限规则表
+        'AUTH_RULE' => 'think_auth_rule',*/
+        // 用户信息表
+        'AUTH_USER' => 'user',
+
+    ),
 );
