9d403e25e0887f64f32ff621016a89440c87d850
krisz <646904527@qq.com>
2016-07-18 14:54:10 +0800

    修改

--- Application/Runtime/Cache/Admin/9dbbfe30e143f14f59a24933acd055a0.php
+++ Application/Runtime/Cache/Admin/9dbbfe30e143f14f59a24933acd055a0.php
@@ -58,8 +58,8 @@
 					<td><input name="" type="checkbox" value=""></td>
 					<td>1</td>
 					<td><input type="text" class="input-text text-c" value="1"></td>
-					<td><img src="temp/brand/dongpeng.jpeg"></td>
-					<td class="text-l"><img title="国内品牌" src="images/gq/cn.gif"> 东鹏</td>
+					<td><img src="/2/QAQproject/Public/admin/temp/brand/dongpeng.jpeg"></td>
+					<td class="text-l"><img title="国内品牌" src="/2/QAQproject/Public/admin/images/gq/cn.gif"> 东鹏</td>
 					<td class="text-l">东鹏陶瓷被评为“中国名牌”、“国家免检产品”、“中国驰名商标”、http://www.dongpeng.net/</td>
 					<td class="f-14 product-brand-manage"><a style="text-decoration:none" onClick="product_brand_edit('品牌编辑','codeing.html','1')" href="javascript:;" title="编辑"><i class="Hui-iconfont">&#xe6df;</i></a> <a style="text-decoration:none" class="ml-5" onClick="active_del(this,'10001')" href="javascript:;" title="删除"><i class="Hui-iconfont">&#xe6e2;</i></a></td>
 				</tr>
