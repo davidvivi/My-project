1c151cca9b14e46fca5ff207a5387eb21bfb154a
huanghaoran <6911293011@qq.com>
2016-07-18 10:54:44 +0800

    home

---
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,38 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html>
+	<head>
+		
+            <title>QAQ数码商城首页</title>
+        
+        <meta charset="utf-8" />
+		
+		<style src='/QAQproject/Public/Home/css/bootstrap.min.css'></style>
+        <!-- 预留接口，等子模板使用 -->
+        
+		
+	
+	</head>
+	
+	<body>
+		<!-- ======页面头部======= -->
+		<div>
+			<ul class="nav nav-tabs">
+				<li role="presentation" class="active"><a href="#">Home</a></li>
+				<li role="presentation"><a href="#">Profile</a></li>
+				<li role="presentation"><a href="#">Messages</a></li>
+			</ul>
+		</div>
+		
+		<!-- ======主体部分接口======= -->
+        
+	
+	
+		
+		<!-- ========页面尾部========== -->
+        <div>wei</div>
+	</body>
+	
+	
+	
+	
+</html>
