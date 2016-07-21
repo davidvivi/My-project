7d59a93f1b11b40e1bf3b76c67f5cbb5648533b6
krisz <646904527@qq.com>
2016-07-21 22:26:23 +0800

    订单

--- Application/Admin/Controller/OrderController.class.php
+++ Application/Admin/Controller/OrderController.class.php
@@ -6,6 +6,9 @@ use Think\Controller;
 
 class OrderController extends CommonController {
 
+    /*
+    *   订单列表
+    */
     public function index()
     {
         $order = M('order');
@@ -14,7 +17,7 @@ class OrderController extends CommonController {
         $payment = array('货到付款','微信支付','支付宝','银联支付');
         $shipping = array('顺丰','圆通','申通','中通');
 
-        $orderlist = M('order')->field('id,uid,numid,buy,written,emailno,consignee,address,tel,orderstatus,addtime,paystatus,payment,shipping')->select();
+        $orderlist = M('order')->field('orderid,uid,numid,buy,written,emailno,consignee,address,tel,orderstatus,addtime,paystatus,payment,shipping')->select();
         //dump($orderlist);
 
         foreach($orderlist as $k=>$v){  
@@ -27,8 +30,63 @@ class OrderController extends CommonController {
         //dump($orderlist);
 
         $this->assign('orderlist',$orderlist);
-
         $this->display('order/order-list');
             
     }
+
+
+    /*
+    *   订单详情
+    */
+    public function order_detail()
+    { 
+        //获取查看的订单的id
+        $orderid = I('get.orderid');
+        //dump($orderid);
+        $order = M('order');
+        $shipping = array('顺丰','圆通','申通','中通');
+        $orderstatus = array('新订单','已发货','已收货','无效订单');
+
+        //根据orderid查询出来一组数据
+        $order = M('order')->where("orderid = $orderid")->find();
+        $order['orderstatus'] = $orderstatus[$order['orderstatus']];
+        $order['shipping'] = $shipping[$order['shipping']];
+
+
+        
+        $uid = $order['uid'];
+        //查询会员信息
+        $user = M('user')->where("id = $uid")->find();
+        $this->assign('user',$user);
+        $this->assign('order',$order);
+
+        $this->display();
+    }
+    
+    /*
+    * 订单编辑
+    */
+    public function order_edit()
+    {    
+        $orderid = I('get.orderid');
+        $order = M('order');
+        $order = M('order')->where("orderid = $orderid")->find();
+        $shipping = array('顺丰','圆通','申通','中通');
+        $orderstatus = array('新订单','已发货','已收货','无效订单');
+
+        $order['orderstatus'] = $orderstatus[$order['orderstatus']];
+        
+        //$order['shipping'] = $shipping[$order['shipping']];
+    
+        $this->assign('shipping',$shipping);
+        $this->assign('order',$order);
+
+        $this->display();
+    }
+
+
+    public function next()
+    { 
+    }
+
 }
