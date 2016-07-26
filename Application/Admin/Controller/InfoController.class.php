5cbda1b92001c428cabca9f19c99675e76c95302
zhangjie <1404386825@qq.com>
2016-07-26 22:15:56 +0800

    公告

--- Application/Admin/Controller/InfoController.class.php
+++ Application/Admin/Controller/InfoController.class.php
@@ -13,64 +13,56 @@ class InfoController extends CommonController
 		$show = $Page->show();// 分页显示输出// 进行分页数据查询 注意limit方法的参数要使用Page类的属性
 		$list = $info->order('addtime')->limit($Page->firstRow.','.$Page->listRows)->select();
 		
-		//格式化时间戳
+		//时间戳转换，状态对应
 		foreach($list as $key=>$value){
 			$list[$key]['addtime'] = date("Y-m-d H:i:s",$list[$key]['addtime']);
-			
+            if($value['state'] == 1){ 
+                $list[$key]['state'] = '开启';
+            }else{ 
+                $list[$key]['state'] = '关闭';
+            }
 		}
-		
-		
 		$this->assign('list',$list);// 赋值数据集
 		$this->assign('page',$show);// 赋值分页输出
 		$this->assign('count',$count);  //总条数
 		$this->display('Info/index'); // 输出模板
 	}
-	
-	/*public function add(){
-		if(IS_POST){
-			echo 1;exit;
-		}
-		$info['contents'] = I('post.title');
-		$info['url'] = I('post.url');
-		$info['state'] = I['post.state'];
-		$info['addtime'] = time();
-		$map = $info;
-		
-		$s = D('information')->add($map);
-		//echo D('information')->getLastsql();exit;
-		if($s){
-			$this->success('插入成功',U('__URL__/index'));
-		}else{
-			$this->error();
-		}
-	}*/
-	
-	public function infoEditForm()
+    public function infoDelete()
     { 
         $id = I('id');
+        $info = M('information');
+        if($id){ 
+            $dd = $info->where("id='{$id}'")->delete();
+            if($dd){ 
+                $this->ajaxReturn('1');
+            }else{ 
+                $this->ajaxReturn('0');
+            }
+        }else{ 
+            $this->ajaxReturn('0');
+        }
+
+    }
+	
+	public function infoAdd()
+    {
+        $this->display('Info/gonggao-add');
+	}
+	
+	public function infoForm()
+    { 
+        
         $url = I('url');
         $contents = I('contents');
         $state = I('state');
-        $link = M('link');
-        // 如果之前有同样的状态存在，把它变为0
-        if($state != 0){
-            $linklist = $link->field('id')->where("state='{$state}'")->select();
-           
-            $linkid = $linklist[0]['id'];
-            $da['state'] = 0;
-            $dl = $link->where("id='{$linkid}'")->save($da);
-        }
-              
+        $information = M('information');
+        $info = M('information');    
         $data['url'] = $url;
         $data['contents'] = $contents;
         $data['state'] = $state;
-        // 如果有id，则是编辑否则是添加
-        if($id){
-            $du = $link->where("id='{$id}'")->save($data);
-        }else{ 
-            $du = $link->add($data);
-        }
-        if($du){
+        $data['addtime'] = time();        
+        $dd = $info->add($data);     
+        if($dd){
             $this->ajaxReturn('1');
         }else{ 
             $this->ajaxReturn('0');
--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,10 +3,10 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-		
+		// 公告
 		$info = D('Information');
-		$list = $info ->field('url','contents')->limit(4)->select();
-		//友情链接
+		$list = $info->field('url,contents')->where('state=1')->order('addtime desc')->limit(6)->select();
+		// 友情链接
         $link = M('link');
         $data['state'] = array('GT',0);
         $link_list = $link ->field('contents,url')->where($data)->select();
