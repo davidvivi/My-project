fd8620945323ca1606439f9905440b6535001b54
krisz <646904527@qq.com>
2016-07-27 20:34:10 +0800

    前台轮播图 个人中心

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -6,12 +6,24 @@ class IndexController extends Controller {
 		// 公告
 		$info = D('Information');
 		$list = $info->field('url,contents')->where('state=1')->order('addtime desc')->limit(6)->select();
-		// 友情链接
+		
+
+        // 友情链接
         $link = M('link');
         $data['state'] = array('GT',0);
         $link_list = $link ->field('contents,url')->where($data)->select();
-        $this->assign('link',$link_list);
+        
 
+        //轮播图
+        $imgturn = M('imgturn');
+        $img_list = $imgturn ->field('imgurl')->order('state')->where('state>0')->select();
+        //将二维数组转换成一位数组
+        $imgurl = array_column($img_list,'imgurl');
+        
+
+
+        $this->assign('img',$imgurl);
+        $this->assign('link',$link_list);
 		$this->assign('list',$list);
 		$this->display('Index/index');
     }
