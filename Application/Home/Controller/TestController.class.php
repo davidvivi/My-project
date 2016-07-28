0b672a74fcfc4fbe359738866e44372f362b2c3e
huanghaoran <6911293011@qq.com>
2016-07-28 23:27:19 +0800

    商品管理基本完成

--- Application/Home/Controller/TestController.class.php
+++ Application/Home/Controller/TestController.class.php
@@ -4,7 +4,25 @@ namespace Home\Controller;
 use Think\Controller;
 class TestController extends Controller {
     public function index(){
-		$this->display('Base/index');
+		$this->display('Test/index');
 		
     }
+	
+	public function upload(){   
+		$upload = new \Think\Upload();// 实例化上传类    
+		$upload->maxSize = 3145728 ;// 设置附件上传大小  
+		$upload->exts = array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+		$upload->saveName = time().'_'.mt_rand(); //文件名
+		$upload->rootPath = './public/img/';
+		$upload->savePath = ''; // 设置附件上传目录    
+		// 上传文件    
+		$info = $upload->upload(); 
+		if(!$info) {
+			// 上传错误提示错误信息  
+			$this->error($upload->getError());    
+		}else{
+			print_r($info);
+			 
+		}
+	}
 }
